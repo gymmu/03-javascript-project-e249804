8b4c242de66e93f7b56fcfa3711cf9a419635369
Stefani Anastasova <e249804@sbl.ch>
2023-10-30 12:07:25 +0100

    aufgabe 1 fertig

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+    if (currentElement === 'e') {
+      result.push("")
+    } else if ( currentElement === 'E') {
+      result.push("")
+
+  } else {
+  result.push(currentElement)
   }
+}
+  return result.join("")
+}
+
+
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0 ; i < input.lenght; i++) {
+    const currentElement = Input [i]
+
+    // Wandle das aktuelle Element in einen Grossbuchstaben um
+   const capitalizedElement = currentElement.toUpperCase()
+   result.push(capitalizedElement)
+}
   return result.join("")
 }
