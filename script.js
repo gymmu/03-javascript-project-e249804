c1ce3de45be5c3354b4a3cd0c5406898bdc1d1e6
Stefani Anastasova <e249804@sbl.ch>
2023-11-13 11:56:20 +0100

    aufgabe 12

--- script.js
+++ script.js
@@ -21,12 +21,13 @@ export function aufgabe01(args) {
 
 
 export function aufgabe02(args) {
+  // erstellt die Funktion 
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
   const currentElement = input[i]
-    // Wandle das aktuelle Element in einen Grossbuchstaben um
+    // Element in einen Grossbuchstaben umwandeln
   const capitalizedElement = currentElement.toUpperCase()
   
   // Hänge den Grossbuchstaben an das Resultat an...
@@ -40,12 +41,19 @@ return result.join("")
 export function aufgabe03(args) {
   const input = args
   const result = []
+  let count= 0
 
-  for (let i = 0 ; i < input.lenght; i++) {
-    const currentElement = input [i]
+  for (let i = 0 ; i < input.length; i++) {
+    const currentElement =input[i]
 
+if (currentElement === 'e') {
+  count=count + 1
+} 
+if (currentElement === 'E') {
+  count = count + 1
 }
-  return result.join("")
+}
+  return count ("")
 }
 
 
@@ -53,9 +61,123 @@ export function aufgabe04(args) {
   const input = args
   const result = []
 
-  for (let i = 0 ; i < input.lenght; i++) {
+  for (let i = 0 ; i < input.length; i++){
+    const currentElement =input[i]
+
+  }
+  return result.join("")
+}
+
+
+export function aufgabe05(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0 ; i < input.length; i++) {
     const currentElement = Input [i]
 
 }
   return result.join("")
 }
+
+
+export function aufgabe06(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0 ; i < input.length; i++) {
+    const currentElement =input[i]
+
+}
+  return result.join("")
+}
+
+
+export function aufgabe07(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0 ; i < input.length; i++) {
+    const currentElement =input[i]
+
+}
+  return result.join("")
+}
+
+
+export function aufgabe09(args) {
+const input = args
+const result = []
+
+let len = 0
+
+for (let i = 0; i < input.length; i++) {
+const currentElement = input[i]
+
+len = len + 1
+
+}
+
+if ( len === 6 ) {
+  return true 
+} else {
+  return false
+  }
+}
+
+
+export function aufgabe11 (args) {
+const input = args
+let ascii = null
+
+if (input.length > 1 ) { // funktionswächter//
+
+  return null 
+}
+
+
+
+for (let i = 0;1 < input.length; i++) {
+const currentElement = input[i]
+ascii = currentElement.charCodeAt(0)
+return ascii
+}
+return null
+}
+
+export function aufgabe12 (args) {
+  const input = args
+  
+  for (let i = 0; i < input.length; i++) {
+const currentElement = input[i]
+
+if (currentElement === "e") {
+  return i
+}
+
+  }
+  
+  return -1
+}
+
+export function aufgabe13 (args) {
+  const input = args
+  const result = []
+  
+  return result.join("")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
